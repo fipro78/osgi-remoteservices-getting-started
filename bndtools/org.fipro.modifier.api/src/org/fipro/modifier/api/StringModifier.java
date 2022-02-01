445b554af3cd7c1604e3741c31ca8d635135cb4d
Dirk Fauth <dirk.fauth@gmail.com>
2022-02-01 23:14:15 +0100

    Added JAX-RS example

--- bndtools/org.fipro.modifier.api/src/org/fipro/modifier/api/StringModifier.java
+++ bndtools/org.fipro.modifier.api/src/org/fipro/modifier/api/StringModifier.java
@@ -1,5 +1,15 @@
 package org.fipro.modifier.api;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/modify")
 public interface StringModifier {
-    String modify(String input);
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{value}")
+    String modify(@PathParam("value") String input);
 }
---
+++ bndtools/org.fipro.modifier.uppercase/src/org/fipro/modifier/uppercase/UppercaseModifier.java
@@ -0,0 +1,35 @@
+package org.fipro.modifier.uppercase;
+
+import java.util.Locale;
+
+import org.fipro.modifier.api.StringModifier;
+import org.osgi.service.component.annotations.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//The JAX-RS path annotation for this service
+@Path("/modify")
+//The OSGi DS component annotation
+@Component(
+    immediate = true,
+    property = { 
+        "service.exported.interfaces=*",
+        "service.exported.intents=jaxrs",
+        "ecf.jaxrs.server.pathPrefix=/upper"})
+public class UppercaseModifier implements StringModifier {
+
+    @GET
+    // The JAX-RS annotation to specify that JSON is produced
+    @Produces(MediaType.APPLICATION_JSON)
+    // The JAX-RS annotation to specify that the last part
+    // of the URL is used as method parameter
+    @Path("/{value}")
+    @Override
+    public String modify(@PathParam("value") String input) {
+        return input.toUpperCase(Locale.getDefault());
+    }
+}
